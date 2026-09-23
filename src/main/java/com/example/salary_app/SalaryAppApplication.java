6af7a13825cb8b42bc7f5b583b4e32859e395db5
uk6655 <k.umemoto06252@gmail.com>
2026-09-23 22:51:54 +0900

    Initial Spring Boot project

---
+++ src/main/java/com/example/salary_app/SalaryAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.salary_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryAppApplication.class, args);
+	}
+
+}
